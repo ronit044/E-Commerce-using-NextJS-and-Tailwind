23e96c51160dfdf0ecb85374344ebf851b67c6b3
Ronit Sharma <ronitsharma3870@gmail.com>
2024-01-07 22:46:12 +0530

    not needed 2

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     images: {
       domains: ['fakestoreapi.com'], // Add the domain(s) from which you are fetching images
     },
-    output:"export",
+    
   };
   
   module.exports = nextConfig;
