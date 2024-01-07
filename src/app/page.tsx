84fdb5cb5e534d97b66e9fa0f2f1baaa37a7b3e4
Ronit Sharma <ronitsharma3870@gmail.com>
2024-01-07 22:51:51 +0530

    process env issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ import Link from "next/link";
 import { ProductTileProps } from '@/components/Product-display/product-type';
 
 async function fetchProductData(): Promise<ProductTileProps[]> {
-  const res = await fetch(`process.env.NEXT_PUBLIC_PRODUCTS_API`);
+  const apiUrl = process.env.NEXT_PUBLIC_PRODUCTS_API ?? "";
+  const res = await fetch(apiUrl);
   const data = await res.json();
   return data;
 }
